362b4b2be1caf2d80ac3f4604ccda4b08986b6a1
Oskari Noppa <oskari@arpakuut.io>
2021-08-26 16:14:30 +0300

    Upgrade devDependencies

--- tests/snapshots.spec.js
+++ tests/snapshots.spec.js
@@ -1,4 +1,4 @@
-const babel = require('babel-core');
+const babel = require('@babel/core');
 const path = require('path');
 
 const should = (/**@type {string}*/descr, /**@type {string}*/js) => it('should ' + descr, () => {
